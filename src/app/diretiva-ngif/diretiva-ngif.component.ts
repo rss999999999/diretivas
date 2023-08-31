a76a70209e27cefb82bd86d2251ccdbd0b416155
Rodrigo <rss.999999999@gmail.com>
2023-08-31 00:43:33 -0300

    #19: Angular CLI: Usando pré-processadores (Sass, Less, Stylus)

--- src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+  styleUrls: ['./diretiva-ngif.component.scss']
 })
 export class DiretivaNgifComponent {
 
